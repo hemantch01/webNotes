ddda30a39374637c9fd46b27189f4fd7610759c9
hemantch01 <hemantchaudhary905@gmail.com>
2025-01-21 17:32:24 +0000

    added signin/signup route

--- backend/src/routes/mainRoute.ts
+++ backend/src/routes/mainRoute.ts
@@ -1,6 +1,8 @@
 import z from 'zod';
 import { Router } from "express";
 import { users } from '../db/usersDB';
+import jwt from 'jsonwebtoken';
+import {jwtPassword} from '../config/config'
 const router = Router();
 
 const signupSchema = z.object({
@@ -13,7 +15,9 @@ router.post('/signup',async (req,res)=>{
     if(!success){
         res.status(411).json({msg:"error in inputs(email must be valid and password min of 8 letters)"})
     }
-    const isUserExist = await users.findOne({email: req.body.email,password: req.body.password})
+    try{ 
+        
+        const isUserExist = await users.findOne({email: req.body.email,password: req.body.password})
     if(isUserExist){
         res.status(403).json({msg:"user already exist"})
     }
@@ -23,8 +27,32 @@ router.post('/signup',async (req,res)=>{
             res.status(200).json({msg:"user created success fully"});
         }
     }
+    } catch(err){
+    res.status(500).json({msg:"internal server error"});
+    }
+   
+});
+const signInSchema = z.object({
+    email:z.string(),
+    password:z.string()
 })
-
-router.post('/signin')
+router.post('/signin',async (req,res)=>{
+    const {success} = signInSchema.safeParse(req.body);
+    if(!success){
+        res.status(403).json({msg:'Invalid Inputs'});
+    }
+    try{
+    const isUserExist = await users.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+    const emailToSign = req.body.email;
+    if(!isUserExist){res.status(403).json('email already exist')};
+    const signInToken= jwt.sign({payload: {emailToSign}},jwtPassword);
+    res.status(200).json({msg:'user Signin successfully',token: signInToken})
+}catch(err){
+    res.status(500).json({msg:'something went wrong'})
+}
+})
 
 export default router;
